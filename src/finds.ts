ad9f2be443a10c6af17b3e62a4aa9a0ae5cf5942
Gandalf2532 <fgier2010@gmail.com>
2026-01-29 21:38:13 +0100

    anfang

--- src/finds.ts
+++ src/finds.ts
@@ -1,3 +1,41 @@
 import "./finds.css";
+import "@mdi/font/css/materialdesignicons.min.css";
+import getApiEndpoint from "./settings";
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const fundliste = document.getElementById("fundliste");
+    if (!fundliste) {
+        return;
+    }
+    const funde = await getFunde();
+    fundliste.innerHTML = `
+    
+    `
+
+});
+
+interface Fund {
+    title: string;
+    //noch nicht vollständig!!!
+}
+
+async function getFunde(): Promise<Array<Fund>> {
+    try {
+        const res = await fetch(`${getApiEndpoint()}/api/submissions`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ "jwt_token": localStorage.getItem("jwt_token")}),
+        });
+
+        const data = await res.json();
+
+        if (data.success) {
+            console.log(data)
+        } else {
+            console.log("Get unsuccessful: ", data);
+        }
+    } catch (err) {
+        console.error("Fetch error:", err);
+    }
+    return new Array<Fund>;
+}
