01a670a9aa9030461d2180861b91776d3640b0da
Gandalf2532 <179391414+Gandalf2532@users.noreply.github.com>
2026-01-30 21:22:58 +0000

    chore: apply prettier formatting

--- src/finds.ts
+++ src/finds.ts
@@ -7,26 +7,30 @@ import getApiEndpoint from "./settings";
 document.addEventListener("DOMContentLoaded", async () => {
     let map: any;
     const mapElement = document.getElementById("map");
-        if (mapElement) {
-            map = L.map("map").setView([51.9481, 10.2651], 6);
-    
-            L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-                attribution:
-                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            }).addTo(map);
-        };
+    if (mapElement) {
+        map = L.map("map").setView([51.9481, 10.2651], 6);
+
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+    }
     const funde = await getFunde();
     funde.forEach((fund) => {
-        let marker = L.marker([fund.coordinate.lat, fund.coordinate.lon], {title: fund.title, riseOnHover: true, riseOffset: 500});
+        let marker = L.marker([fund.coordinate.lat, fund.coordinate.lon], {
+            title: fund.title,
+            riseOnHover: true,
+            riseOffset: 500,
+        });
         marker.bindPopup(`
             <div class="title">${fund.title}</div>
             <div class="description">${fund.description}</div>
             <div class="date">${fund.timestamp}</div>
         `);
-        marker.on('mouseover', () => {
+        marker.on("mouseover", () => {
             marker.openPopup();
         });
-        marker.on('mouseout', () => {
+        marker.on("mouseout", () => {
             marker.closePopup();
         });
 
@@ -75,7 +79,7 @@ async function getFunde(): Promise<Array<Fund>> {
             let funde: Array<Fund> = [];
             data.data.forEach((el: Fund) => {
                 funde.push(el);
-            })
+            });
             return funde;
         } else {
             console.error("Get unsuccessful: ", data);
