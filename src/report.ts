e7118aec313d93be199aa47b12afbcf0db761ced
lumentae <66220444+lumentae@users.noreply.github.com>
2026-02-01 04:05:29 +0100

    Forgot other map style

--- src/report.ts
+++ src/report.ts
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }).addTo(map);
     } else {
         L.maplibreGL({
-            style: `${getApiEndpoint()}/styles/basic-preview/style.json`,
+            style: `${getApiEndpoint()}/styles/osm-liberty/style.json`,
         }).addTo(map);
     }
 
