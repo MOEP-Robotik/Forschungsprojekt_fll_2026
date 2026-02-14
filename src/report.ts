2dcb47ec430509789e7b90dd57fc10e5e43dffc5
lumentae <me@lumentae.dev>
2026-02-14 11:25:16 +0100

    Add comment field

--- src/report.ts
+++ src/report.ts
@@ -141,6 +141,9 @@ async function sendReport(event: Event) {
     const datierungsInput = document.getElementById(
         "datierung",
     ) as HTMLSelectElement;
+    const commentInput = document.getElementById(
+        "comment",
+    ) as HTMLInputElement;
     const lengthInput = document.getElementById("length") as HTMLInputElement;
     const widthInput = document.getElementById("width") as HTMLInputElement;
     const heightInput = document.getElementById("height") as HTMLInputElement;
@@ -214,6 +217,7 @@ async function sendReport(event: Event) {
     formData.append("size", JSON.stringify(size));
     formData.append("material", materialInput.value);
     formData.append("datierung", datierungsInput.value);
+    formData.append("comment", commentInput.value);
     if (dateInput.value) {
         formData.append("date", dateInput.value);
     }
