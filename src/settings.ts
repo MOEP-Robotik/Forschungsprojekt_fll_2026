f9e5a4a5a400b8de7a44209a1e3720db69a162d2
MOEP-Robotik <240936838+MOEP-Robotik@users.noreply.github.com>
2026-02-23 13:46:37 +0000

    chore: apply prettier formatting

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,9 @@ export function setApiEndpoint(endpoint: string): void {
     localStorage.setItem("api_endpoint", endpoint);
 }
 export function getApiEndpoint(): string {
-    return localStorage.getItem("api_endpoint") ?? "http://192.168.204.217:8000";
+    return (
+        localStorage.getItem("api_endpoint") ?? "http://192.168.204.217:8000"
+    );
 }
 
 export function setUseRasterTiles(use: boolean): void {
